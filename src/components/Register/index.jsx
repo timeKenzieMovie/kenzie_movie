aa3cfe7e5449e64bc321fdcbe50238368b82515d
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2023-10-30 19:11:50 -0300

    feat: criação da estrutura da página de cadastro

---
+++ src/components/Register/index.jsx
@@ -0,0 +1,13 @@
+export const RegisterPage = () => {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>Cadastro</h1>
+                    <p>Preencha os campos para cadastrar-se</p>
+                </div>
+                <div>Sair</div>
+            </div>
+        </section>
+    )
+}
