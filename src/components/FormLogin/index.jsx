f90b612e1f47166c9bff051c143eaed3b4a59757
JardelMD <130687030+JardelMD@users.noreply.github.com>
2023-10-31 15:17:08 -0300

    feat: Criando e estilizando página de Login

--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom"
 import { Input } from "../Input"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { formLoginSchema } from "./formLoginSchema"
+import styles from "./style.module.scss"
+
 
 export const FormLogin = () => {
 
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(formLoginSchema),
+    })
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
     return (
-        <section>
-            <h1 >Login</h1>
-            <form >
+        <section className={styles.formLogin}>
+            <h1 className="title1">Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                 <Input
                     type="email"
                     id="email"
@@ -19,12 +31,12 @@ export const FormLogin = () => {
                     {...register("password")}
                     error={errors.password}
                 />
-                <div>
-                    <button type="submit">Entrar</button>
-                    <p>ou</p>
-                    <Link to={"/register"}><h3 type="submit" >Cadastre-se</h3></Link >
-                </div>
             </form>
+            <div className={styles.divRegisterLogin}>
+                <button className={styles.buttonLogin} type="submit">Entrar</button>
+                <p className="paragraph">ou</p>
+                <Link className="menuItem link" to={"/register"}><h3 type="submit" >Cadastre-se</h3></Link >
+            </div>
         </section>
     )
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,24 +1,28 @@
+import { Link } from "react-router-dom"
 import { FormLogin } from "../FormLogin"
 import styles from "./style.module.scss"
 
 export const Login = () => {
     return (
-        <section>
-            <header>
-                <div>
-                    <p className={styles.logo}>kenziemovie</p>
-                    <div>
-                        <span>Cadastre-se</span>
-                        <button>Entrar</button>
+        <section className={styles.login}>
+            <div className={styles.pageLogin}>
+                <header className={styles.header}>
+                    <div className={styles.divHeader}>
+                        <p className={styles.logo}>kenziemovie</p>
+                        <div className={styles.divNav}>
+                            <Link className="menuItem" to={"/register"}><span >Cadastre-se</span></Link>
+
+                            <button className={styles.buttonLogin}>Entrar</button>
+                        </div>
                     </div>
-                </div>
-            </header>
-            <main>
-                <FormLogin />
-                <hr className={styles.hr} />
-            </main>
-            <footer>
-                <h3>Todos os direitos reservados - Kenzie Academy Brasil</h3>
+                </header>
+                <main className={styles.main}>
+                    <FormLogin />
+                </main>
+            </div>
+            <hr className={styles.hr} />
+            <footer className={styles.footer}>
+                <h3 className="title1-mobileB center">Todos os direitos reservados - Kenzie Academy Brasil</h3>
             </footer>
         </section>
     )
