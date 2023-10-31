91f32b6109a2bbad75685222aeb83ab3a0a4cc7b
JardelMD <130687030+JardelMD@users.noreply.github.com>
2023-10-31 12:27:42 -0300

    fix: Corrigindo erro da página not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import "./styles/index.scss"
-=======
 import { RoutesMain } from "./routes/RoutesMain"
->>>>>>> 52ac97468e400a90ba7d85b90f57a5ebcaf77101
 
 function App() {
 
--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import { NotFoundPage } from "../pages/NotFoundPage"
+import { NotFoundPage } from "../pages/NotFound"
 import { LoginPage } from "../pages/Login"
 import { RegisterPage } from "../pages/Register"
 import { LandingPage } from "../pages/LandingPage"
