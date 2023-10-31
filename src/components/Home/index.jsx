f4797580b3802f8d01642d51edb2d49df468a881
nayaragracioli <nayaragraciolisa@gmail.com>
2023-10-31 14:37:04 -0300

    feat:criação do componente Home

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const Home = () => {
+    const [isVisibleCreate, setIsVisibleCreate] = useState(false);
+    const [isVisibleEdit, setIsVisibleEdit] = useState(false);
+
+    
+    return(
+        <h1>oi</h1>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import { Home } from "../../components/Home";
+import kenzieMovieIcon from "../../assets/kenziemovie.svg";
+import Style from "./style.module.scss";
+
 // Página interna com a lógica para estar logado ou não
 export const HomePage = () => {
+    const [user, setUser] = useState(null);
+
     return (
         <>
-            <p>HomePage Atualizada</p>
+            <header className={Style.internalPage}>
+                <div>
+                    <img src={kenzieMovieIcon} alt="Kenzie Movie Icon" />
+                    <div>
+                        <p className={`buttonSmall`}>J</p>
+                        {/* <h3>{user?.name}</h3> */}
+                        <h3 className="menuItem">José da Silva</h3>
+                        <button className="title2" type="submit">Sair</button>
+                    </div>
+                </div>
+            </header>
+            <main className={Style.internalPage}>
+                <Home />
+            </main>
+
         </>
     )
 }
