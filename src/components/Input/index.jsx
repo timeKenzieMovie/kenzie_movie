4c84b4fc445be4076b75f0c5270496a36f56daa4
JardelMD <130687030+JardelMD@users.noreply.github.com>
2023-11-01 13:52:08 -0300

    fix: resolvendo conflitos da homepage

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { forwardRef } from "react";
-=======
-import { forwardRef } from "react"
->>>>>>> 4641fb0e6bcfa13654c5edf75fabd23743b45058
 
 export const Input = forwardRef(({ children, error, ...rest }, ref) => {
     return (
