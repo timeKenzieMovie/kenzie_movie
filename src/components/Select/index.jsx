2e7738f51313cde04579db97e5f14be65d6a6683
JardelMD <130687030+JardelMD@users.noreply.github.com>
2023-11-01 20:34:33 -0300

    feat: Estilizando modais

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -4,9 +4,9 @@ import { forwardRef } from "react";
 export const Select = forwardRef (({error, children, label, id, ...rest}, ref) => {
 
     return (
-        <div >
+        <div>
             <label className={`headline`} htmlFor={id}>{label}</label>
-            <select ref={ref} {...rest} id={id}>{children}</select>
+            <select className="select"  ref={ref} {...rest} id={id}>{children}</select>
             <span>{error && error.message}</span>
         </div>
     )
