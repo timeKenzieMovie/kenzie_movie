93e4a7072285d145ca2473b7d1efeac1fee69841
JardelMD <130687030+JardelMD@users.noreply.github.com>
2023-11-01 15:24:44 -0300

    feat: Estilizando HomePage

--- src/components/Home/HomeCard/index.jsx
+++ src/components/Home/HomeCard/index.jsx
@@ -2,6 +2,7 @@ import { MdEdit, MdDelete } from "react-icons/md";
 import { FiStar } from "react-icons/fi";
 import { CreateAvaliationModal } from "./HomeCardModal/CreateAvaliationModal";
 import { EditAvaliationModal } from "./HomeCardModal/EditAvaliationModal";
+import Style from "./style.module.scss"
 
 export const HomeCard = ({ avaliationList, setIsVisibleCreate, isVisibleCreate, isVisibleEdit, setIsVisibleEdit }) => {
 
@@ -25,24 +26,26 @@ export const HomeCard = ({ avaliationList, setIsVisibleCreate, isVisibleCreate, 
         //     ))}
         // </ul>
         //</div>
-        <div>
-            <ul >
+        <div className={Style.myAvaliation}>
+            <ul>
                 <li>
                     <p>Sua avaliação</p>
                     <div>
-                        <div>
+                        <div className={Style.divAvaliation}>
                             <p className={`paragraph`}>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
                         </div>
-                        <div>
+                        <div className={Style.noteEditRemove}>
                             <p className={`title1-mobileB`}><FiStar color="var(--yellow)" /> 5.0</p>
-                            <button onClick={() => { setIsVisibleEdit(true); setEditingTech(avaliation) }} aria-label="edit"><MdEdit size={17} color="var(--yellow)" /></button>
-                            <button onClick={() => techDelete(avaliation.id)} aria-label="delete"><MdDelete size={17} color="var(--yellow)" /></button>
+                            <div>
+                                <button onClick={() => { setIsVisibleEdit(true); setEditingTech(avaliation) }} aria-label="edit"><MdEdit size={17} color="var(--yellow)" /></button>
+                                <button onClick={() => techDelete(avaliation.id)} aria-label="delete"><MdDelete size={17} color="var(--yellow)" /></button>
+                            </div>
                         </div>
                     </div>
                 </li>
             </ul>
-            {isVisibleCreate ? <CreateAvaliationModal setIsVisibleCreate={setIsVisibleCreate}/> : null}
-            {isVisibleEdit ? <EditAvaliationModal setIsVisibleEdit={setIsVisibleEdit}/> : null}
+            {isVisibleCreate ? <CreateAvaliationModal setIsVisibleCreate={setIsVisibleCreate} /> : null}
+            {isVisibleEdit ? <EditAvaliationModal setIsVisibleEdit={setIsVisibleEdit} /> : null}
         </div>
     )
 }
