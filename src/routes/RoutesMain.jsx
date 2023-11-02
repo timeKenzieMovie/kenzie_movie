96b64d79e5d6a3f7e54fed0da05f5f89ba659525
nayaragracioli <nayaragraciolisa@gmail.com>
2023-11-02 08:21:19 -0300

    feat: Colocando o movieList e o useEffect do LandingPage no RoutesMain

--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -1,18 +1,34 @@
 import { Route, Routes } from "react-router-dom"
 import { NotFoundPage } from "../pages/NotFound"
 
-import { LandingPage } from "../pages/LandingPage"
-import { HomePage } from "../pages/Home"
-import { LoginPage } from "../pages/LoginPage"
-import { RegisterPage } from "../pages/RegisterPage"
+import { LandingPage } from "../pages/LandingPage";
+import { HomePage } from "../pages/Home";
+import { LoginPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 
 
 export const RoutesMain = () => {
+    const [ moviesList, setMoviesList ] = useState([]);
+
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                const { data } = await api.get("movies?_embed=reviews");
+                setMoviesList(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+        getMovies();
+    }, []);
+
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/" element={<LandingPage />} />
+            <Route path="/" element={<LandingPage moviesList={moviesList}/>} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/*" element={<NotFoundPage />} />
         </Routes>
